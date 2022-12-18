d8efb5bdaf9dfb6b32272d05b8d0a68928c8dd03
Feuill0u <65813825+Feuill0u@users.noreply.github.com>
2022-12-18 21:28:40 +0100

    improve run.sh

--- web/noideachall/run.sh
+++ web/noideachall/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t firstchall --build-arg host=$1 ./webapp
-docker run -d --name firstchallweb -p 5000:5000 firstchall
-docker build -t bot --build-arg host=$1 ./bot
-docker run -d --name firstchallbot bot
+docker build -t noidea --build-arg host=$1 ./webapp
+docker run -d --name noidea -p 5000:5000 noidea
+docker build -t noidea_bot --build-arg host=$1 ./bot
+docker run -d --name noidea_bot noidea_bot
