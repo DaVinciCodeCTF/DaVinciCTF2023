20f30cb78a35f7938e4016b475493cede78a4e13
Feuill0u <thomas.guillemet@edu.devinci.fr>
2023-01-12 18:26:45 +0100

    fix run.sh

--- web/noideachall/run.sh
+++ web/noideachall/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t noidea --build-arg ./webapp
+docker build -t noidea ./webapp
 docker run -d --name noidea -p 5000:5000 noidea
 docker build -t noidea_bot --build-arg host=$1 ./bot
 docker run -d --name noidea_bot noidea_bot
