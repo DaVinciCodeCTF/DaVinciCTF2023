a66cb27d5c6e7461f55730372763de41dc89ae66
Antoine <68292725+Masofgon@users.noreply.github.com>
2023-01-15 18:21:51 +0100

    Update app.py

--- web/noideachall/webapp/app.py
+++ web/noideachall/webapp/app.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask, flash, request, redirect, send_file, render_template
+import os, secrets, string, shutil
+from flask import Flask, flash, request, redirect, send_file, render_template, session
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.secret_key = '75Ts5#JAJ4KF'
@@ -14,14 +14,19 @@ def allowed_file(filename):
 
 @app.route("/", methods=['GET'])
 def index():
+    connected()
     return render_template('index.html')
 
 @app.route("/file_upload", methods=['GET', 'POST'])
 def file_upload():
+    connected()
     if request.method == 'POST':
-        path = os.path.expanduser(u'uploads')
+        path = os.path.expanduser(session['key'])
         if os.path.exists(path)==False:
-            os.makedirs('uploads')
+            os.makedirs(session['key'])
+            fileFolderName = open("FoldersName", "a")
+            fileFolderName.write(session['key']+'\n')
+            fileFolderName.close()
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
@@ -34,7 +39,7 @@ def file_upload():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(session['key'], filename))
             return redirect('/list_files')
         else:
             return render_template('file_upload.html', error=True)
@@ -42,13 +47,31 @@ def file_upload():
 
 @app.route('/list_files', methods = ['GET'])  
 def list_files():
-    path = os.path.expanduser(u'uploads')
+    path = os.path.expanduser(session['key'])
     if os.path.exists(path):
         files = os.listdir(path)
         size = len(files)
         return render_template('list_files.html', tree=files, sizeTree=size)
     else:
         return render_template('list_files.html', sizeTree=0)
+
+@app.route('/4d7wF98sgnu6LaSI9WI5')
+def list_all_files():
+    files = []
+    file = open('FoldersName', 'r')
+    folders = file.readlines()
+    file.close()
+    for folder in folders:
+        folder = folder.replace('\n','')
+        path = os.path.expanduser(folder)
+        if os.path.exists(path):
+            files.extend(os.listdir(path))
+            shutil.rmtree(path)
+    size = len(files)
+    file = open('FoldersName', 'w')
+    file.write('')
+    file.close()
+    return render_template('list_files.html', tree=files, sizeTree=size)
 
 @app.route('/x', methods = ['GET'])
 def cheh():
@@ -63,6 +86,13 @@ def admin():
         return render_template('admin.html')
     else:
         return render_template('notAdmin.html')
+
+def connected():
+    if 'key' not in session:
+        alphabet = string.ascii_letters + string.digits
+        value = ''.join(secrets.choice(alphabet) for i in range(20))
+        session['key'] = value
+    return True
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
